d74ecbee19db3c761953195f37b7a6ba1a64f11d
honestit <michal.kupisinski@gmail.com>
2021-03-24 18:38:21 +0100

    Podstawowa struktura projektu (po utworzeniu w oparciu o spring initializr)

---
+++ projects/ams/src/main/java/com/projects/ams/AdvertsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.projects.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ projects/ams/src/test/java/com/projects/ams/AdvertsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertsManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
