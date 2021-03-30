f5e3f6e2b298b21c9c9c6831144edf4c54494c0e
honestit <michal.kupisinski@gmail.com>
2021-03-30 18:29:53 +0200

    Obsługa żądania GET do strony głównej

--- projects/ams/src/main/java/com/projects/ams/controllers/HomePageController.java
+++ projects/ams/src/main/java/com/projects/ams/controllers/HomePageController.java
@@ -1,11 +1,31 @@
 package com.projects.ams.controllers;
 
+import com.projects.ams.model.domain.Advert;
+import com.projects.ams.model.repositories.AdvertRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 @Slf4j
 public class HomePageController {
+
+    private AdvertRepository advertRepository;
+
+    public HomePageController(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @GetMapping
+    public String prepareHomePage(Model model) {
+        List<Advert> adverts = advertRepository.findAllByOrderByPostedDesc();
+        log.debug("Collected {} adverts for home page", adverts.size());
+        model.addAttribute("adverts", adverts);
+        return "home-page";
+    }
 }
--- projects/ams/src/main/java/com/projects/ams/model/repositories/AdvertRepository.java
+++ projects/ams/src/main/java/com/projects/ams/model/repositories/AdvertRepository.java
@@ -3,5 +3,8 @@ package com.projects.ams.model.repositories;
 import com.projects.ams.model.domain.Advert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
+    List<Advert> findAllByOrderByPostedDesc();
 }
