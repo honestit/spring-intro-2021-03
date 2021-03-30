fbea60ce2482955e123c7944e6e9a98d99045dbc
honestit <michal.kupisinski@gmail.com>
2021-03-30 19:23:28 +0200

    Komponent tworzący testowe dane na start aplikacji

---
+++ projects/ams/src/main/java/com/projects/ams/config/StartupDataLoader.java
@@ -0,0 +1,40 @@
+package com.projects.ams.config;
+
+import com.projects.ams.model.domain.Advert;
+import com.projects.ams.model.domain.User;
+import com.projects.ams.model.repositories.AdvertRepository;
+import com.projects.ams.model.repositories.UserRepository;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class StartupDataLoader {
+
+    private UserRepository userRepository;
+    private AdvertRepository advertRepository;
+
+    public StartupDataLoader(UserRepository userRepository, AdvertRepository advertRepository) {
+        this.userRepository = userRepository;
+        this.advertRepository = advertRepository;
+    }
+
+    @EventListener
+    public void loadData(ContextRefreshedEvent event) {
+        User user = new User(null, "test", "{noop}test", "test", "test", true);
+        userRepository.save(user);
+
+        Advert advert1 = new Advert(null, "Kupię psa", "Kupię mądrego psa",
+                LocalDateTime.of(2021, 3, 19, 12, 0), user);
+        Advert advert2 = new Advert(null, "Sprzedam psa", "Miał być mądry, ale nie jest",
+                LocalDateTime.of(2021, 3, 21, 12, 0), user);
+        Advert advert3 = new Advert(null, "Kupię kota", "Nie chce psa, ale chcę kota",
+                LocalDateTime.of(2021, 3, 10, 12, 0), user);
+
+        advertRepository.save(advert1);
+        advertRepository.save(advert2);
+        advertRepository.save(advert3);
+    }
+}
