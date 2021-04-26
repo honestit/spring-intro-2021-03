f3d0ef2611f76103314d5c00a0c5ccd7b4a7eae0
honestit <michal.kupisinski@gmail.com>
2021-04-26 20:24:25 +0200

    Pobieranie pojedynczego użytkownika w stylu REST

--- projects/ams/src/main/java/com/projects/ams/rest/UsersController.java
+++ projects/ams/src/main/java/com/projects/ams/rest/UsersController.java
@@ -1,15 +1,17 @@
 package com.projects.ams.rest;
 
-// Kontroler w stylu REST odwołuje się do zasobu, który obsługuje.
-// W przeciwieństwie do konstrolera w stylu MVC, który odwołuje się do strony, którą obsługę; bez znaczenia, jakie zasoby na tej stronie są przetwarzane.
 
 import com.projects.ams.model.domain.User;
 import com.projects.ams.model.repositories.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
+
+// Kontroler w stylu REST odwołuje się do zasobu, który obsługuje.
+// W przeciwieństwie do konstrolera w stylu MVC, który odwołuje się do strony, którą obsługę; bez znaczenia, jakie zasoby na tej stronie są przetwarzane.
 
 @RestController
 // Używamy przedrostku "/api" dla ścieżek, aby je odróżnić od ścieżek używanych przez kontrolery MVC
@@ -26,5 +28,17 @@ public class UsersController {
     // Lista użytkowników (List<User>) zostanie automatycznie zserializowana do formatu JSON
     public List<User> getUsers() {
         return userRepository.findAllWithDataBy();
+    }
+
+//  /api/users/{id}
+    @GetMapping("/{id}")
+    // Adnotacja @PathVariable powoduje, że w parametrze metody "id" znajdzie się wartość fragmentu ścieżki określona jako "{id}". Dla ścieżki "/api/users/2" będzie to wartość "2"
+    public User getUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User doesn't exist");
+        }
     }
 }
