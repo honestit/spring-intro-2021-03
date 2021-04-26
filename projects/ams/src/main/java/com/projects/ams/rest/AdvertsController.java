58ceab0fe88595853b8e7f9158f3ccf294d2bd6d
honestit <michal.kupisinski@gmail.com>
2021-04-26 20:40:12 +0200

    Pobieranie pojedynczego ogłoszenia w stylu REST

--- projects/ams/src/main/java/com/projects/ams/rest/AdvertsController.java
+++ projects/ams/src/main/java/com/projects/ams/rest/AdvertsController.java
@@ -1,12 +1,17 @@
 package com.projects.ams.rest;
 
 import com.projects.ams.model.domain.Advert;
+import com.projects.ams.model.domain.User;
 import com.projects.ams.model.repositories.AdvertRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/adverts")
@@ -21,5 +26,15 @@ public class AdvertsController {
     @GetMapping
     public List<Advert> getAdverts() {
         return advertRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Advert getUser(@PathVariable Long id) {
+        Optional<Advert> optionalAdvert = advertRepository.findById(id);
+        if (optionalAdvert.isPresent()) {
+            return optionalAdvert.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User doesn't exist");
+        }
     }
 }
